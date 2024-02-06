34b9bbaa55f01cc16ecd942de1d4abb08a06feb7
Conrad Lundberg <conrizzo@gmail.com>
2024-02-06 22:33:07 +0100

    update cors, message posting

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 # GNU nano 6.2                                                                                                        app.py                                                                                                                 from flask import Flask, request
-from flask import Flask # this is for debugging on local server
-from flask import Flask, request # this is for debugging on local server
+
+from flask import Flask, request  # this is for debugging on local server
 
 from flask_limiter import Limiter
 from flask_cors import CORS
 from flask_limiter.extension import RateLimitExceeded
+import json
 
 import storage_data
 
 app = Flask(__name__)
-CORS(app)
 
+# "http://localhost:8080" only for testing in the cors
+cors = CORS(app, resources={r"/back_end/api/*": {"origins": ["https://conradswebsite.com"]}})
 
 limiter = Limiter(
     app=app,
@@ -21,19 +23,33 @@ limiter = Limiter(
 # Initialize the counter
 get_data_counter = int(storage_data.read_file())
 
-
 @app.errorhandler(RateLimitExceeded)
 def ratelimit_handler(e):
     return {'message': 'Rate limit exceeded, the limit is 3 queries per 10 seconds.'}, 429
-
 
 @app.route('/back_end/api/data')
 @limiter.limit("3/10seconds")
 def get_data():
     global get_data_counter
-    get_data_counter += 1
+    get_data_counter+=1
     storage_data.write_file(str(get_data_counter))
     return {'message': f'The backend server says hello back! The number of queries is: {get_data_counter}'}
+
+
+@app.route('/back_end/api/leave_message', methods=['POST'])
+@limiter.limit("1/30seconds")
+def leave_message():
+    data = request.get_json()
+    name = data.get('name')
+    subject = data.get('subject')
+    message = data.get('message')
+    message_dict = {"name": name,"subject": subject, "message": message}
+    with open('user_messages.json', 'a') as file:
+        file.write(json.dumps(message_dict) + '\n')
+    return jsonify({'message': 'Message received'}), 200
+
+
+
 
 
 # this is for debugging without frontend server
@@ -43,5 +59,5 @@ def home():
 
 
 if __name__ == '__main__':
-   # app.run(host='0.0.0.0')
-   app.run(host='127.0.0.1', debug=True) # this is for debugging without frontend server
+    # app.run(host='0.0.0.0')    
+    app.run(host='127.0.0.1', debug=True) # this is for debugging without frontend server
