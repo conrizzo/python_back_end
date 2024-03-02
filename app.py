3478ba53874b32b198de316c75404fc543475c58
Conrad Lundberg <conrizzo@gmail.com>
2024-03-02 13:08:20 +0100

    simple cleanup on app.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-# GNU nano 6.2                                                                                                        app.py                                                                                                                 from flask import Flask, request
-
+from flask import Flask, request
 from datetime import datetime
-
-
-from flask import Flask, request  # this is for debugging on local server
-
+from datetime import datetime
+from flask import Flask, request, jsonify, send_file
 from flask_limiter import Limiter
 from flask_cors import CORS
 from flask_limiter.extension import RateLimitExceeded
-import json
-
 import storage_data
+import cosine_similarity
+import json
+import bleach
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/back_end/api/*": {"origins": [
+            "https://conradswebsite.com", "https://project.conradswebsite.com"]}})
 
-# "http://localhost:8080" only for testing in the cors
-cors = CORS(app, resources={r"/back_end/api/*": {"origins": ["https://conradswebsite.com","http://localhost:8080"]}})
 
 limiter = Limiter(
     app=app,
@@ -23,18 +21,21 @@ limiter = Limiter(
     default_limits=["3 per 10 seconds"]
 )
 
+
 # Initialize the counter
 get_data_counter = int(storage_data.read_file())
+
 
 @app.errorhandler(RateLimitExceeded)
 def ratelimit_handler(e):
     return {'message': 'Rate limit exceeded, the limit is 3 queries per 10 seconds.'}, 429
 
+
 @app.route('/back_end/api/data')
 @limiter.limit("3/10seconds")
 def get_data():
     global get_data_counter
-    get_data_counter+=1
+    get_data_counter += 1
     storage_data.write_file(str(get_data_counter))
     return {'message': f'The backend server says hello back! The number of queries is: {get_data_counter}'}
 
@@ -46,32 +47,44 @@ def leave_message():
     name = data.get('name')
     subject = data.get('subject')
     message = data.get('message')
-    
+
+    # Sanitize the inputs with bleach
+    name = bleach.clean(name)
+    subject = bleach.clean(subject)
+    message = bleach.clean(message)
+
     current_datetime = datetime.now()
     date_string = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    
-    message_dict = {"date": date_string, "name": name,"subject": subject, "message": message}
+
+    message_dict = {"date": date_string, "name": name,
+                    "subject": subject, "message": message}
     with open('user_messages.json', 'a') as file:
         file.write(json.dumps(message_dict) + '\n')
-    return jsonify({'message': 'Message received'}), 200
+    return jsonify({'message': 'Form submission successful'}), 200
 
 
 @app.route('/back_end/api/cosine', methods=['POST'])
 @limiter.limit("1/10seconds")
 def get_cosine_similarity():
     data = request.get_json()
-    s1 = data.get('sentence1')
-    s2 = data.get('sentence2')
+    s1, s2 = data.get('sentence1'), data.get('sentence2')
+    s1, s2 = bleach.clean(s1), bleach.clean(s2)
     result = cosine_similarity.compute_similarity(s1, s2)
     return jsonify(result)  # Convert the dictionary to a JSON response
 
 
-# this is for debugging without frontend server
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/back_end/api/download')
+@limiter.limit("1/30seconds")
+def download_file():
+    path = "test100.txt"
+    return send_file(path, as_attachment=True)
+
+### This is for debugging without another frontend server
+### The host must be local for this work 
+#@app.route('/')
+#def home():
+#    return "Hello, World!"
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')    
-    app.run(host='127.0.0.1', debug=True) # this is for debugging without frontend server
+    app.run(host='0.0.0.0')
